9448f531210382038c7765a340d1eed280faa80f
Rohan <rkamraly@uwo.ca>
2021-12-05 14:27:41 -0500

    updated to work without sequelize

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,11 +3,4 @@ module.exports = {
     USER: "root",
     PASSWORD: "root",
     DB: "valT",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    }
 };
