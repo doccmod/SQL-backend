7a958d63cce0d6b99a2d3500db6116c68ac1b1f7
Rohan <rkamraly@uwo.ca>
2021-12-05 13:44:55 -0500

    Changed server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const db = require("./app/models");
 
 const app = express();
 
@@ -12,6 +13,9 @@ app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true}));
+
+const db = require("./app/models");
+db.sequelize.sync();
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to the application."});
