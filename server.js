111303b9279665424e1e466eb7d32e1d10e3d37c
Rohan <rkamraly@uwo.ca>
2021-12-05 16:00:50 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.get("/", (req, res) => {
     res.json({message: "Welcome to the application."});
 });
 
+require("./app/routes/user.routes.js")(app);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
