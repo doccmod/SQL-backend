7f259453a7585b32e6046f31c3e494c935e7c719
emachej <emachej@uwo.ca>
2021-12-05 15:04:28 -0500

    user creation function added

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,9 +1,20 @@
 const sql = require("./db.js");
 
 const user = function(User) {
-    this.title = User.title;
-    this.description = User.description;
-    this.published = User.published;
+    this.name = User.name;
+    this.email = User.email;
+    this.password = User.password;
 };
 
-//user.create = ()
+User.create = (newUser, result) => {
+    sql.query("INSERT INTO user", newUser, (err, res) => {
+      if (err) {
+        console.log("error: ", err);
+        result(err, null);
+        return;
+      }
+  
+      console.log("created user: ", { id: res.insertId, ...newUser });
+      result(null, { id: res.insertId, ...newUser });
+    });
+  };
