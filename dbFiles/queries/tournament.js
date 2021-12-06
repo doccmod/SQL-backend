87e77333bbfd10fff1690c382c9e499453155243
emachej <emachej@uwo.ca>
2021-12-06 13:38:40 -0500

    added tournament query

---
+++ dbFiles/queries/tournament.js
@@ -0,0 +1,27 @@
+// Gets the dbConfig and uses mysql2
+const sql = require("../config/dbConfig.js");
+mysql = require('mysql2');
+
+
+const TournamentList = async() => {
+    try {
+        let tournamentList = sql.promise().query(
+        `SELECT startDate,
+        FROM Tournament,
+        WHERE region = ${tRegion}`
+        );
+
+        return tournamentList;
+    } catch {
+        console.log(error);
+    }
+}
+
+
+
+
+
+
+module.exports = {
+    TournamentList
+}
