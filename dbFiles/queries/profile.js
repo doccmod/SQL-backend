0ae02d7d8f423b75a06f8dde0c1d6a4e814ac000
Rohan <rkamraly@uwo.ca>
2021-12-06 13:32:52 -0500

    added queries to profile.js

---
+++ dbFiles/queries/profile.js
@@ -0,0 +1,36 @@
+// Gets the dbConfig and uses mysql2
+const sql = require("../config/dbConfig.js");
+mysql = require('mysql2');
+
+const GetWins = async(username) => {
+    try {
+
+        let getWins = sql.promise().query(`SELECT pWin
+        FROM Player
+        WHERE username = ${username}`);
+        
+        return getWins;
+
+    } catch {
+        console.log(error);
+    }
+}
+
+const GetLosses = async(username) => {
+    try {
+        let getLoss = sql.promise().query(`SELECT pLoss
+        FROM Player
+        WHERE username = ${username}`);
+
+        return getLoss;
+        
+    } catch {
+        console.log(error);
+    }
+}
+
+module.exports = {
+    GetWins,
+    GetLosses,
+    GetTeamName
+}
